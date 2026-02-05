a4fdcb14b428a8494be4e3847e9ec938759ecdb8
Sean Ryan <seanryan895@gmail.com>
2026-02-05 10:37:56 -0500

    step1

--- whiteboard.m
+++
@@ -1,6 +0,0 @@
-clear all; 
-wd = '/mnt/weka/home/80030361/ETHOS_Simulations/1194203/Pancreas/Session_1/sct';
-wd = '/home/80030361/ETHOS_Simulations/EthosExports/1194203/Pancreas/Session_1/sct'
-rtplan = fullfile(wd,'RTPLAN.1.2.246.352.800.5261851517227480225.9292172278832325515'); 
-
-rtplan = dicomread(rtplan); 
